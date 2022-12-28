626b59f48cb38fe87c60a1bae743eca2932de4d4
Darshani <dbarasker@systango.com>
2022-12-28 16:50:49 +0530

    added changes for dynamicloading of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ComponentRef, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { HelloComponent } from './component/hello/hello.component';
+import { PlaceHolder } from './PlaceHolder.directive';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamicloading';
+  componentRef!: ComponentRef<HelloComponent>;
+  // @ViewChild('template',{read: ViewContainerRef})
+  // viewTemplate!: ViewContainerRef;
+
+  @ViewChild(PlaceHolder,{static:true})
+  alertHost!: PlaceHolder;
+  private messages:{type:Type<HelloComponent>}[]=[
+    {
+      type:HelloComponent
+    }
+  ];
+  currentIndex!: number;
+  constructor(private cfr:ComponentFactoryResolver)
+  {
+
+  }
+
+/*
+ Prior to Angular 13
+*/
+  // loadComponent(): void
+  // {
+  //   const componentFactory = this.cfr.resolveComponentFactory(HelloComponent);
+  //   this.componentRef = this.viewTemplate.createComponent(componentFactory);
+  //   this.componentRef.instance.name='SuccessFully';
+  // }
+
+
+  /*
+  Angular 13+ 
+  dynamic component creation via ViewContainerRef.createComponent does not require
+   resolving component factory: component class can be used directly.
+  */
+  loadComponent(): void
+  {
+    this.alertHost.viewContainerRef?.clear();
+    const componentRef=this.alertHost.viewContainerRef?.createComponent(HelloComponent);
+    componentRef.instance.name='Successfully';
+  }
+
+  removeComponent(): void
+  {
+    this.alertHost.viewContainerRef?.clear();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './component/hello/hello.component';
+import { PlaceHolder } from './PlaceHolder.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    PlaceHolder
   ],
   imports: [
     BrowserModule,
